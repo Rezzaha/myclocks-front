dc38453599c8dd86e564234e9b39811eb530c336
Rezzaha <160749896+Rezzaha@users.noreply.github.com>
2026-04-06 18:41:59 +0200

    Add files via upload

---
+++ carousel/arrow-icons.tsx
@@ -0,0 +1,39 @@
+import Iconify, { IconifyProps } from '../iconify';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  icon?: IconifyProps; // Right icon
+  isRTL?: boolean;
+};
+
+export function LeftIcon({ icon = 'eva:arrow-ios-forward-fill', isRTL }: Props) {
+  return (
+    <Iconify
+      icon={icon}
+      sx={{
+        width: '30px',
+        height: '30px',
+        transform: ' scaleX(-1)',
+        ...(isRTL && {
+          transform: ' scaleX(1)',
+        }),
+      }}
+    />
+  );
+}
+
+export function RightIcon({ icon = 'eva:arrow-ios-forward-fill', isRTL }: Props) {
+  return (
+    <Iconify
+      icon={icon}
+      sx={{
+        width: '30px',
+        height: '30px',
+        ...(isRTL && {
+          transform: ' scaleX(-1)',
+        }),
+      }}
+    />
+  );
+}
